699b0473918f7db24ece756bb2a371cf8c321356
doolingdavidrs21 <david@rs21.io>
2025-12-09 16:05:39 -0700

    updated chapter9

--- chapter9/complete/notebooks/swc_simple_client.py
+++ chapter9/complete/notebooks/swc_simple_client.py
@@ -10,22 +10,24 @@ LIST_TEAMS_ENDPOINT = "/v0/teams/"
 LIST_WEEKS_ENDPOINT = "/v0/weeks/"
 GET_COUNTS_ENDPOINT = "/v0/counts/"
 
-logger = logging.getLogger(__name__) 
+base_url = "http://localhost:8000"
+
+logger = logging.getLogger(__name__)
 
 @backoff.on_exception(
-    wait_gen=backoff.expo, 
-    exception=(httpx.RequestError, httpx.HTTPStatusError),  
-    max_time=5,  
-    jitter=backoff.random_jitter  
+    wait_gen=backoff.expo,
+    exception=(httpx.RequestError, httpx.HTTPStatusError),
+    max_time=5,
+    jitter=backoff.random_jitter
 )
 def call_api_endpoint(
     base_url: str,
-    api_endpoint: str, 
+    api_endpoint: str,
     api_params: dict = None
 ) -> httpx.Response:
 
     try:
-        with httpx.Client(base_url=base_url) as client: 
+        with httpx.Client(base_url=base_url) as client:
             logger.debug(f"base_url: {base_url}, api_endpoint: {api_endpoint}")
             response = client.get(api_endpoint, params=api_params)
             response.raise_for_status()
@@ -33,7 +35,7 @@ def call_api_endpoint(
             return response
     except httpx.HTTPStatusError as e:
         logger.error(f"HTTP status error occurred: {e.response.text}")
-        return httpx.Response(status_code=e.response.status_code, 
+        return httpx.Response(status_code=e.response.status_code,
                               content=b"API error")
     except httpx.RequestError as e:
         logger.error(f"Request error occurred: {str(e)}")
